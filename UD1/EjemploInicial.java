c91b20ab6de42663e7766c94004385d968d36d81
FRV <romerovicentefrancisco@gmail.com>
2025-09-19 14:52:31 +0200

    Ejemplos de variables

---
+++ UD1/EjemploInicial.java
@@ -0,0 +1,25 @@
+package UD1;
+
+public class EjemploInicial {
+
+    public static void main(String args[]){
+        
+        int edad = 13;
+        int edadInicial = 5;
+        int edadPadres = 69;
+        float peso = 69.420f;
+        double pi = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679d;
+
+        //String es un tipo de dato complejo por eso se escribe en mayusculas al no ser primitivo. (Es un objeto)
+
+        String welcome = "Bienvenidos al curso de DAW 2025/2026"; //Comillas dobles
+        char letraInicial = 'F'; //Comillas simples
+
+        boolean meMuero = true; //Me muero
+
+        //Aquí se escribe el código del programa.
+        System.out.print(welcome);
+
+    }
+    
+}
