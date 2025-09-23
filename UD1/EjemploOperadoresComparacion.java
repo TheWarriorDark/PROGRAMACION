90e5c5145a0c960d2ad7a97f779bfba4b650794e
FRV <romerovicentefrancisco@gmail.com>
2025-09-23 13:04:42 +0200

    Clase 23/09/25 	modified:   UD1/EjemploOperadoresComparacion.java

--- UD1/EjemploOperadoresComparacion.java
+++ UD1/EjemploOperadoresComparacion.java
@@ -8,19 +8,59 @@ public class EjemploOperadoresComparacion {
         
         boolean puedeIrDelante = false;
 
-        int altura = 0;
+        //En una linea se pueden declarar variables del mismo tipo.
+        int altura, edad = 0;
 
-        Scanner teclado = new Scanner(System.in); //comienza la comunicación con el teclado
+        Scanner teclado = new Scanner(System.in); //comienza la comunicación con el teclado.
 
         System.out.print("Escribe la altura del ninio en cm: ");
-        altura = teclado.nextInt(); //Coge lo que hemos introducido y lo mete en la variable altura
+        altura = teclado.nextInt(); //Coge lo que hemos introducido y lo mete en la variable altura.
         
-        if (altura >= 135){
+        System.out.print("Escribe la edad: ");
+        edad = teclado.nextInt(); //Coge lo que hemos introducido y lo mete en la variable altura.
+
+        if (altura >= 135 || edad >= 10  ){
             puedeIrDelante = true; 
         }
 
-        System.out.println("La condición de si puede ir delante es: " + puedeIrDelante);
 
-        teclado.close(); //cierra la comunicación con el teclado
+
+        System.out.println("La condicion de si puede ir delante es: " + puedeIrDelante);
+
+        /*
+         * Icaro para poder volar desde la isla de creta hasta atenas necesitaba
+         * Pesar menos de 80.5 kg y cumplir una de las siguientes dos condiciones
+         * Hacer caso a su padre o tener mas de rango 5 en google fit
+         */
+
+         float pesoIcaro = 80.4f;
+         boolean casoPadre = false;
+         int rangoGoogleFit = 10;
+
+         if (pesoIcaro < 80.5 && (casoPadre == true || rangoGoogleFit > 5)){
+
+            System.err.println("Llego a Atenas");
+
+         } else {
+
+            System.err.println("No llego a Atenas");
+         }
+
+         /*
+          * Para volver a por las llaves que se le habían olvidado en el laberinto
+          * Tiene que hacer caso a su padre y su pero sea distinto de 90 kgs
+          * o solo con tener un nivel de google fit mayor a 10 tambien valdría
+          */
+
+         if (rangoGoogleFit > 10 || (pesoIcaro != 90 || casoPadre == true)){
+
+            System.err.println("consiguio las llaves");
+
+         } else {
+
+            System.err.println("No las consiguio :c");
+         }
+
+        teclado.close(); //cierra la comunicación con el teclado.
     }
 }
